61c8f27baa4ed01530de35a6cb1ec76a97362393
Saikumar Kasaboina <sk19306@corp.ojas-it.com>
2020-07-30 09:20:57 +0530

    task

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +9,49 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
-
-  constructor(private activatedRoute: ActivatedRoute) { }
+  today: string;
+  profileForm: FormGroup;
+  constructor(private activatedRoute: ActivatedRoute,private formBuilder: FormBuilder,private router: Router) {
+    
+   }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-  }
+    const now = new Date();
+   this.today = now.toISOString();
 
+   this.profileForm = this.formBuilder.group({
+    name: ['',],
+    email: [''],
+    visit: [''],
+    pVisit: [''],
+    date: [''],
+    entry: [''],
+    exit: ['']
+});
+
+  }
+  
+  
+  get f() { return this.profileForm.controls; }
+  myDate: String = new Date().toLocaleDateString();
+  
+  submit(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+       
+        name: this.profileForm.controls['name'].value,
+        email: this.profileForm.controls['email'].value,
+        visit: this.profileForm.controls['visit'].value,
+        pVisit: this.profileForm.controls['pVisit'].value,
+        date: this.profileForm.controls['date'].value,
+        entry: this.profileForm.controls['entry'].value,
+        exit: this.profileForm.controls['exit'].value,
+
+      }
+    };
+    
+    this.router.navigate(['/home'],navigationExtras);
+  }
+  
 }
